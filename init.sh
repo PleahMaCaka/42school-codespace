5449b60fc82700a4bea4bdca072ad7fbd4a77554
SEOA7777 <pleahmacaka@gmail.com>
2023-12-20 01:55:34 +0000

    Register alias in bashrc

--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
+# Function to add alias if it doesn't exist
+add_alias() {
+    if ! grep -q "alias $1=" ~/.bashrc; then
+        echo "alias $1=\"$2\"" >> ~/.bashrc
+    fi
+}
+
 # Change mode and register in alias for simple script
 chmod 777 ./run.sh
-alias run=./run.sh
-
-alias norm="norminette *.c *.h"
-alias cls=clear
+add_alias run ./run.sh
+add_alias norm "norminette *.c *.h"
+add_alias cls clear
+add_alias init ./init.sh
 
 # Install Norminette
 python3 -m pip install --upgrade pip setuptools
@@ -11,3 +18,7 @@ python3 -m pip install norminette
 
 # Create Makefile
 cmake .
+
+# Shell script is running on subshell
+# For uisng alias in comportable, just run bash
+bash
